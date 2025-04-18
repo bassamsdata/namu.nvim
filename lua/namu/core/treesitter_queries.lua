781f9ce8bf3fbb34cf03368fdc441128001069ef
Bassam Data <basssamdata@outlook.com>
2025-04-18 17:34:40 -0400

    feat(treesitter): adding javascript own query

--- lua/namu/core/treesitter_queries.lua
+++ lua/namu/core/treesitter_queries.lua
@@ -1,6 +1,9 @@
+-- @thanks to @stevearc and Aerial.nvim for those quiers as they are lirteral
+-- copy paste. I found that "locals" make it very hard when dealing
+-- with javascript and typescript
+-- I need to do more queries but needs to learn it first :)
 local M = {}
 
--- TypeScript/JavaScript query
 M.typescript = [[
 (function_signature
   name: (identifier) @name
@@ -76,6 +79,74 @@ M.typescript = [[
   (#set! "kind" "Function")) @symbol @selection
 ]]
 
+M.javascript = [[
+(class_declaration
+  name: (identifier) @name
+  (#set! "kind" "Class")) @symbol
+
+(function_declaration
+  name: (identifier) @name
+  (#set! "kind" "Function")) @symbol
+
+(generator_function_declaration
+  name: (identifier) @name
+  (#set! "kind" "Function")) @symbol
+
+(method_definition
+  name: (property_identifier) @name
+  (#set! "kind" "Method")) @symbol
+
+(field_definition
+  property: (property_identifier) @name
+  value: (arrow_function)
+  (#set! "kind" "Method")) @symbol
+
+; const fn = () => {}
+(lexical_declaration
+  (variable_declarator
+    name: (identifier) @name
+    value: [
+      (arrow_function)
+      (function_expression)
+      (generator_function)
+    ] @symbol)
+  (#set! "kind" "Function")) @start
+
+; describe("Unit test")
+(call_expression
+  function: (identifier) @method @name
+  (#any-of? @method "describe" "it" "test" "afterAll" "afterEach" "beforeAll" "beforeEach")
+  arguments: (arguments
+    (string
+      (string_fragment) @name @string))?
+  (#set! "kind" "Function")) @symbol
+
+; test.skip("this test")
+(call_expression
+  function: (member_expression
+    object: (identifier) @method
+    (#any-of? @method "describe" "it" "test")
+    property: (property_identifier) @modifier
+    (#any-of? @modifier "skip" "todo")) @name
+  arguments: (arguments
+    (string
+      (string_fragment) @name @string))?
+  (#set! "kind" "Function")) @symbol
+
+; describe.each([])("Test suite")
+(call_expression
+  function: (call_expression
+    function: (member_expression
+      object: (identifier) @method
+      (#any-of? @method "describe" "it" "test")
+      property: (property_identifier) @modifier
+      (#any-of? @modifier "each")) @name)
+  arguments: (arguments
+    (string
+      (string_fragment) @name @string))?
+  (#set! "kind" "Function")) @symbol
+]]
+
 -- Markdown query
 M.markdown = [[
 (atx_heading
@@ -115,9 +186,6 @@ M.vimdoc = [[
   text: (word) @name
   (#set! "kind" "Interface")) @symbol
 ]]
-
--- JavaScript can use the TypeScript query for now
-M.javascript = M.typescript
 
 -- Get the appropriate query for a filetype
 function M.get_query_for_filetype(filetype)
